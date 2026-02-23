c681fffdd70d0fddba52ec860d6988a539f720ad
alinapavlova27 <apavlova.121207@gmail.com>
2026-02-23 18:06:46 +0300

    Delete 2.0 directory

--- 2.0/lab1_creative.py
+++
@@ -1,7 +0,0 @@
-print ("Добро", end=" ")
-print ("пожаловать!")
-course = 1
-print (f"Студент {course} курса")
-print ("Удачи в обучении на", end=" ")
-print (1, 2, 3, sep=",", end=" ")
-print ("курсах.")
--- 2.0/lab1_part7.py
+++
@@ -1,3 +0,0 @@
-f = open ("output.txt", "w", encoding="utf-8")
-print("Меня зовут Алина, я учусь на биотехнолога", file=f)
-f.close()
